dff2b5d28caaab245d9e738457eba2ed8214eca5
dinzdale <gmjacobs61@gmail.com>
2025-04-12 23:18:09 -0400

    wip 4

--- composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/models/Playlist.kt
+++ composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/models/Playlist.kt
@@ -2,18 +2,19 @@ package com.gmjproductions.blurayplaylist.models
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import nl.adaptivity.xmlutil.serialization.XmlBefore
 import nl.adaptivity.xmlutil.serialization.XmlElement
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
 
 @Serializable
-@SerialName("playlist")
 @XmlSerialName(value = "playlist")
 data class Playlist (
     @XmlElement
     val title: String,
     @XmlSerialName(value = "trackList")
-    val trackList: TrackList
-//    val extension: PlaylistExtension,
+    val trackList: TrackList,
+    @XmlSerialName(value = "extension")
+    val extension: PlaylistExtension,
 //
 //    //@Json(name = "_xmlns")
 //    val xmlns: String,
@@ -25,31 +26,28 @@ data class Playlist (
 //    val version: String
 )
 @Serializable
+@XmlSerialName(value = "extension")
 data class PlaylistExtension (
+    @XmlElement
     val item: List<Item>,
 
     //@Json(name = "_application")
+    @XmlSerialName
     val application: String
 )
 @Serializable
+@XmlSerialName("item", prefix = "vlc")
 data class Item (
     //@Json(name = "_tid")
+    @XmlSerialName
     val tid: String,
 
-    //@Json(name = "__prefix")
-    val prefix: Prefix
+//    //@Json(name = "__prefix")
+//    @XmlElement
+//    val prefix: Prefix
 )
-@Serializable
-enum class Prefix(val value: String) {
-    Vlc("vlc");
 
-    companion object {
-        public fun fromValue(value: String): Prefix = when (value) {
-            "vlc" -> Vlc
-            else  -> throw IllegalArgumentException()
-        }
-    }
-}
+
 @Serializable
 @XmlSerialName(value = "tracklist")
 data class TrackList (
@@ -65,18 +63,12 @@ data class Track (
     val duration: String,
     //val extension: TrackExtension
 )
-@Serializable
-data class TrackExtension (
-    val id: ID,
 
-    //@Json(name = "_application")
-    val application: String
-)
-@Serializable
-data class ID (
-    //@Json(name = "__prefix")
-    val prefix: Prefix,
-
-    //@Json(name = "__text")
-    val text: String
-)
+//@Serializable
+//data class ID (
+//    //@Json(name = "__prefix")
+//    val prefix: Prefix,
+//
+//    //@Json(name = "__text")
+//    val text: String
+//)
