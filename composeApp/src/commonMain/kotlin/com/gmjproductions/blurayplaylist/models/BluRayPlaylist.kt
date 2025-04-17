8dc4c001ca6dabc8f95684311da5a0543d5eab35
dinzdale <gmjacobs61@gmail.com>
2025-04-17 14:44:33 -0400

    Initial BlueRayPlaylist definition

--- composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/models/BluRayPlaylist.kt
+++ composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/models/BluRayPlaylist.kt
@@ -5,11 +5,16 @@ class BluRayPlaylist {
     fun fromVLCPlaylist(playlist: Playlist) : String {
         val byteOS = ByteArrayOutputStream()
         byteOS.writeBytes(getHeader().toByteArray())
-        // (Playlist start) 4 bytes/Wprd boundary - offset from filie beginning
-        // (Playlist Mark start) 4 Bytes/Word boundary - offset from filie beginning
-        // (Extension Data start) 4 Bytes/Word boundary $00000000 - no extension
-        // Reserved 20 Bytes == 0
-        // (Playback type) 1 Byte = value 01 - standard, 02 - Random - 03 - shuffle
+        // $00-$03 (Header) 4 bytes = "MPLS"
+        // $04-$07 (Version Number) 4 bytes = "0200"
+        // $08-$0B (Playlist Start Address) 4 Bytes/Word boundary - offset to Playlist section (from beginning of file)
+        // $0C-$0F (Playlist Mark Start Address) 4 Bytes - offset to PMSA from beginning of file
+        // $10-$13 (Extension Data Section)  4 Bytes - not included: $00000000
+        // $14-$27 (Unused) 14 Bytes = 00 00 00 00 00 00 00
+        // $28-$2B (Application Info Playlist length) 4 bytes - always: 0000000E (14 bytes???)
+        // $2C (unused byte) - 1 Byte = 0
+        // $2D (Playback type) - 1 Byte - $01 = Standard, $02 = Random, $03 = Shuffle Play
+        // $2E-$2F
     }
 
     private fun getHeader() = "MPLS"
