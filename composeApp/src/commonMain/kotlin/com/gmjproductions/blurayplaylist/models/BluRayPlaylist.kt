158190cb8a424593c9513cd95c0f44043c59bdab
dinzdale <gmjacobs61@gmail.com>
2025-04-17 14:53:51 -0400

    Initial BlueRayPlaylist definition 2

--- composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/models/BluRayPlaylist.kt
+++ composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/models/BluRayPlaylist.kt
@@ -14,7 +14,16 @@ class BluRayPlaylist {
         // $28-$2B (Application Info Playlist length) 4 bytes - always: 0000000E (14 bytes???)
         // $2C (unused byte) - 1 Byte = 0
         // $2D (Playback type) - 1 Byte - $01 = Standard, $02 = Random, $03 = Shuffle Play
-        // $2E-$2F
+        // $2E-$2F (Playback count) - $0000 if standard above
+        // $30-$37 (User operation mask table) 8 bytes - Description pending
+        // $38 - (Miscillaneous flags) BTIS SET: 7             6       5         4-0
+        //                                       playlist      sound   lossles   reserved
+        //                                       random access effects sound
+        // $39 - Reserved 1 byte = 0
+        // >>>>PLAYLIST SECTION<<<
+        // >>>>PLAYLIST MARK SECTION<<<
+        // >>>>EXTENSION DATA SECTION<<
+
     }
 
     private fun getHeader() = "MPLS"
