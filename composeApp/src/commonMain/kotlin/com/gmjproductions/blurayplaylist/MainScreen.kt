e72e7884a28b12def39250bbbe536f34c6dbfd51
dinzdale <gmujacobs61@gmail.com>
2025-06-14 22:17:56 -0400

    get filepicker working 4

--- composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/MainScreen.kt
@@ -2,6 +2,7 @@ package com.gmjproductions.blurayplaylist
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,13 +10,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridItemScope
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -25,20 +29,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.darkrockstudios.libraries.mpfilepicker.FilePicker
 import com.gmjproductions.blurayplaylist.models.Calcit
-import com.gmjproductions.blurayplaylist.models.MultiAVCHDItem
-import kotlinx.serialization.decodeFromString
 import nl.adaptivity.xmlutil.serialization.XML
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.xml.sax.XMLReader
-import java.io.BufferedInputStream
-import java.io.FileInputStream
+import java.io.BufferedReader
 import java.io.FileReader
-import java.io.StringReader
-import javax.xml.stream.XMLStreamReader
+import java.util.logging.Logger
 
 
 @Composable
@@ -48,18 +46,26 @@ fun MainScreen() {
             var inputFile by remember { mutableStateOf<String?>(null) }
             var showFilePicker by remember { mutableStateOf(false) }
             var inputSettings by remember { mutableStateOf<Calcit?>(null) }
-            Row(Modifier.fillMaxSize()) {
+
+            Column (Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top) {
                 Header(inputFile) {
                     showFilePicker = true
                 }
+                ShowResults(inputSettings)
             }
             SelectInputFile(showFilePicker) {
                 showFilePicker = false
-                inputFile = it ?: ""
+                inputFile = it
             }
             LaunchedEffect(inputFile) {
                 inputFile?.also {
+
+                    System.out.println("Before Parse")
                     inputSettings = ParseInputFile(it)
+                    System.out.println("After Parse")
+                    inputSettings?.llist?.itemList?.forEach {
+                        System.out.println("${it.ID}: ${it.value}")
+                    }
                 }
             }
         }
@@ -74,8 +80,8 @@ fun SelectInputFile(showFilePicker: Boolean, onFileSelected: (String?) -> Unit) 
 }
 
 fun ParseInputFile(filePath: String?) = filePath?.let {
-    val fis = FileInputStream(filePath)
-    val contents = fis.readAllBytes().toString()
+    val fileReader = FileReader(filePath)
+    val contents = BufferedReader(fileReader).readText()
     XML.decodeFromString<Calcit>(contents)
 
 }
@@ -95,6 +101,21 @@ fun Header(filePath: String?, onOpenFileClick: () -> Unit) {
         Spacer(Modifier.width(10.dp))
         Text(filePath ?: "")
 
+    }
+}
+@Composable
+fun ShowResults(calcit: Calcit?) {
+    calcit?.llist?.itemList?.also {
+        val lazyListState = rememberLazyListState()
+        LazyColumn {
+            items(it) { nxtItem ->
+                Row(Modifier.fillMaxWidth().wrapContentHeight()) {
+                    Text("${nxtItem.ID}: ${nxtItem.value}")
+                }
+            }
+        }
+    } ?: Row(Modifier.fillMaxWidth().wrapContentHeight()) {
+        Text("Empty")
     }
 }
 
--- composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/models/MultiAVCHDProject.kt
+++ composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/models/MultiAVCHDProject.kt
@@ -3,22 +3,30 @@ package com.gmjproductions.blurayplaylist.models
 import kotlinx.serialization.Serializable
 import nl.adaptivity.xmlutil.serialization.XmlElement
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
+import nl.adaptivity.xmlutil.serialization.XmlValue
 
 @Serializable
 @XmlSerialName(value = "CALCIT")
 class Calcit() {
     @XmlElement
-    val l : TheList = TheList()
+    val llist : LList? = null
 }
 
 @Serializable
 @XmlSerialName(value = "L")
-class TheList() {
-    val itemList : ArrayList<MultiAVCHDItem> = arrayListOf()
+class LList() {
+    val itemList : ArrayList<MultiAVCHDItem>? = null
 }
 
 @Serializable
 @XmlSerialName(value = "F")
 class MultiAVCHDItem() {
     val ID : String = ""
+    @XmlValue
+    val value : String = ""
 }
+
+@Serializable
+@XmlSerialName(value = "E")
+class E() {}
+
