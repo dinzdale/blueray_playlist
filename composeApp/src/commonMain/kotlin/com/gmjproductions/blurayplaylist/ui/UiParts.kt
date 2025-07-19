524fe3cdcb235e455bed1313e04a727c040440df
dinzdale <gmujacobs61@gmail.com>
2025-07-19 15:29:59 -0400

    New Ui parts

---
+++ composeApp/src/commonMain/kotlin/com/gmjproductions/blurayplaylist/ui/UiParts.kt
@@ -0,0 +1,84 @@
+package com.gmjproductions.blurayplaylist.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun ActionButton(label: String, onHover: @Composable (() -> Unit)? = null, onClick: () -> Unit) {
+    var showHover by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.wrapContentSize().onPointerEvent(PointerEventType.Enter) {
+        showHover = true
+    }.onPointerEvent(PointerEventType.Exit) {
+        showHover = false
+    }) {
+        if (showHover) {
+            onHover?.invoke()
+        }
+        Button(
+            onClick = onClick,
+            modifier = Modifier.size(width = 50.dp, height = 50.dp)
+        ) {
+            Text(text = label.first().toString())
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PreviewButton() {
+    MaterialTheme {
+        ActionButton("A", { HintBox("Apply conversion") }) {
+
+        }
+    }
+}
+
+@Composable
+fun HintBox(text: String) {
+    Box(
+        modifier = Modifier.width(80.dp).wrapContentHeight().background(Color.Black),
+        Alignment.Center
+    ) {
+        Text(text = text, color = Color.White)
+    }
+}
+
+@Composable
+fun ItemTextField(text: String, placeHolder: String) {
+
+    var value by remember { mutableStateOf(text) }
+    TextField(value, { value = it }, Modifier.background(color = Color.LightGray))
+
+}
+
+@Preview
+@Composable
+fun PreviewItemTextField() {
+    Surface {
+        ItemTextField("Initial value here","Enter text here")
+    }
+}
